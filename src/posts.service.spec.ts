7e6df54ad1c5c849bd8152533948aac59807511e
Vlad <raimnist@yandex.ru>
2026-04-09 00:21:42 +0300

    done

--- src/posts.service.spec.ts
+++ src/posts.service.spec.ts
@@ -14,9 +14,23 @@ describe('PostsService', () => {
 
   it('should add a new post', () => {
     // реализуйте тест-кейс
+    postsService.create(post);
+
+    expect(postsService.find('2')).toEqual({
+      id: '2',
+      text: 'Mocked post',
+      date: expect.any(String),
+    });
   });
 
   it('should find a post', () => {
     // реализуйте тест-кейс
+    postsService.find('1');
+
+    expect(postsService.find('1')).toEqual({
+      id: '1',
+      text: 'Some pre-existing post',
+      date: expect.any(String),
+    });
   });
 });
